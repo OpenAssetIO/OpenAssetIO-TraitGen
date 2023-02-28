6f52e1654125af5b63f4bf261fc77c36a4f1ba32
David Feltell <david.feltell@foundry.com>
2023-02-28 22:09:47 +0000

    squash: Test basic C++ `#include`s
    Signed-off-by: David Feltell <david.feltell@foundry.com>

---
+++ tests/generators/cpp/src/test.cpp
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 The Foundry Visionmongers Ltd
+#include <catch2/catch.hpp>
+
+#include <openassetio_traitgen_test_all/specifications/test.hpp>
+#include <openassetio_traitgen_test_all/traits/aNamespace.hpp>
+#include <openassetio_traitgen_test_all/traits/anotherNamespace.hpp>
