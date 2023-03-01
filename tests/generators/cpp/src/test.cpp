b6a0fd6f57f41282b8d9b21b15a84b9d91c102ee
David Feltell <david.feltell@foundry.com>
2023-03-01 13:30:43 +0000

    squash: Permute possible #include styles in test
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- tests/generators/cpp/src/test.cpp
+++ tests/generators/cpp/src/test.cpp
@@ -2,6 +2,15 @@
 // Copyright 2022 The Foundry Visionmongers Ltd
 #include <catch2/catch.hpp>
 
+#if defined OPENASSETIO_TRAITGENTEST_INCLUDES_PACKAGE
+#include <openassetio_traitgen_test_all/openassetio_traitgen_test_all.hpp>
+#elif defined OPENASSETIO_TRAITGENTEST_INCLUDES_SUBPACKAGE
+#include <openassetio_traitgen_test_all/specifications/specifications.hpp>
+#include <openassetio_traitgen_test_all/traits/traits.hpp>
+#elif defined OPENASSETIO_TRAITGENTEST_INCLUDES_NAMESPACE
 #include <openassetio_traitgen_test_all/specifications/test.hpp>
 #include <openassetio_traitgen_test_all/traits/aNamespace.hpp>
 #include <openassetio_traitgen_test_all/traits/anotherNamespace.hpp>
+#else
+#error "An #include style must be chosen"
+#endif
