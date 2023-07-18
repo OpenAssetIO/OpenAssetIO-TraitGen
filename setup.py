38e95954fe4508ff9a8096600f370e944fd9ce84
Tom Cowland <tom@foundry.com>
2023-07-18 15:51:20 +0100

    [Build] Update `pyyaml` and `conan` versions
    Fixes `AttributeError: cython_source` when trying to install via
    `pyyaml` 6+, however `conan` `1.60.1` only supports `6.0.0` explicitly,
    so we have to pin at that older version.
    
    See: https://github.com/yaml/pyyaml/issues/601
    
    Signed-off-by: Tom Cowland <tom@foundry.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     packages=find_packages(where="python"),
     package_dir={"": "python"},
     include_package_data=True,
-    install_requires=["jinja2==3.1.2", "pyyaml==5.4.1", "jsonschema==4.7.2"],
+    # We are currently constrained to pyyaml=6.0.0 exactly due to conan 1.60.1
+    install_requires=["jinja2==3.1.2", "pyyaml==6.0.0", "jsonschema==4.7.2"],
     entry_points={
         "console_scripts": ["openassetio-traitgen=openassetio_traitgen.__main__:main"],
     },
